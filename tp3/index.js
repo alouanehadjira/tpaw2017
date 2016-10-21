295fad025e113103b54841ce97d9f22ffc8ae7f8
alouanehadjira <alouane.hadjira@gmail.com>
2016-10-21 11:19:18 +0200

    Update index.js

--- tp3/index.js
+++ tp3/index.js
@@ -1,1 +1,18 @@
 $(function(){
+  <div class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="OK" data-dismiss="modal" aria-label="OK"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">MESSAGE MODAL</h4>
+      </div>
+      <div class="modal-body">
+        <p>VEUILLEZ REMPLIR TOUS LES CHAMPS SVP.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
+        
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
