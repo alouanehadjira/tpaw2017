ee1ac24f43b92404b3f0c7a9355805f105412239
alouanehadjira <alouane.hadjira@gmail.com>
2016-11-07 14:51:22 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -1,19 +1,23 @@
 
 window.onload = function(){
-    document.getElementById(“searchCity”).addEventListener(“submit”, function(
-event
-){
-event.preventDefau
-lt(); // pour annuler le rechargement de la page
-        var city = document.getElementById(“city”).value;
+    document.getElementById(“searchCity”).addEventListener(“submit”, function(event){
+event.preventDefault(); // pour annuler le rechargement de la page
+        var city = document.getElementById("city").value;
       searchCity(city);
        });
   }
 function searchCity(_city){
-    console.log(‘searchCity’,‘Hello from ‘+_city);
-    //A compléter dans la suite du TP
-}
+    console.log("searchCity","Hello from "+_city);
+    var resuest=new XMLHttpRequest();
+    Request.open("Get","http://api.openweathermap.org/data/2.5/weather?q=’+_city+’&appid=026b817616733447ff9cf585780ff7af",true);
+    request.onload =function(){
+    if(request.status >=200 && request.status <400){
+    // Success!
+    var resp = request.responseText;
+    }
+        
+        
 function searchLatLng(_lat, _ lng){
-    console.log(searchLatLng,‘Hello from ‘+_lat+’,’+lng);
+    console.log(searchLatLng,"Hello from "+_lat+","+lng);
     //A compléter dans la suite du TP
 }
