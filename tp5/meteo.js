ed739fd3991ffd50566a9cfe23a05cced8ebe959
alouanehadjira <alouane.hadjira@gmail.com>
2016-11-10 10:40:16 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -52,7 +52,8 @@ function searchCity(_city){
 
 function searchLatLng(_lat,_lng){  
        console.log(searchLatLng,"Hello from "+_lat+","+_lng);    //A compléter dans la suite du TP
-         
+        document.getElementById("result").innerHTML ="<h2>"+ _lat+","+ _lng+"</h2>" ;
+    
        var request = new XMLHttpRequest();
        request.open("GET","http://api.openweathermap.org/data/2.5/weather?lat="+_lat+"&lon="+_lng+"&appid=a86ef65f3662cce72b37f1b8af722d94",true);
 
