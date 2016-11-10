25036e0540e14b5a0d7236414c8551f7245f63f6
alouanehadjira <alouane.hadjira@gmail.com>
2016-11-10 12:16:38 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -7,7 +7,7 @@ window.onload = function() {
       searchCity(city);   
     });
 
-
+}
     
 function searchCity(_city){  
        console.log("searchCity","Hello from "+_city);    //A compléter dans la suite du TP
@@ -46,4 +46,38 @@ function searchCity(_city){
 
 
    
+function searchLatLng(_lat,_lng){  
+       console.log(searchLatLng,"Hello from "+_lat+","+_lng);    //A compléter dans la suite du TP
+        document.getElementById("result").innerHTML ="<h2>"+ _lat+","+ _lng+"</h2>" ;
+   
+      var request = new XMLHttpRequest();
+      request.open("GET","https://demo.bilelz.fr/owmap/?lat="+_lat+"&lon="+_lng+"&appid=026b817616733447ff9cf585780ff7af",true);
+
+      request.onload=function (){
+           if (request.status >= 200 && request.status <400){
+               // Success!
+              var responseJSON = JSON.parse(request.responseText);
+             
+
+              //var temperature= responseJSON.temperature.value;              
+           //  document.getElementById("result").innerHTML ="<h2>"+ responseJSON.name+ "</h2>" ;
+
+          //   document.getElementById("temps").innerHTML=  ; 
+             
+          //  document.getElementById("icon").innerHTML= "<img src=http://openweathermap.org/img/w/"+responseJSON.weather[0].icon +".png />";
+             // document.getElementById("temperature").innerHTML=responseJSON.main.temp; 
+             
+              // document.getElementById("cloud").innerHTML=responseJSON.clouds.all; 
+             // document.getElementById("humidity").innerHTML= responseJSON.main.humidity + " % ";
+           }else {
+                 document.getElementById
+           }
+       };
+
+       request.onerror=function(){
+
+       };
+
+       request.send();
+}
 
