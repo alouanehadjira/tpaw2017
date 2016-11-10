7e5c881f52fac6c090390cc2e4af9f14bf463d4e
alouanehadjira <alouane.hadjira@gmail.com>
2016-11-10 10:08:53 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -2,48 +2,14 @@
 window.onload = function() {
     
     document.getElementById("searchCity").addEventListener("submit", function(event){
-      event.preventDefault(); // pour annuler le rechargement de la page      
+      event.preventDefault(); 
       var city = document.getElementById("city").value;
       searchCity(city);   
     });
-    
-    function searchCity(_city){  
-       console.log("searchCity","Hello from "+_city);    
-         var request = new XMLHttpRequest();
-       request.open("get","http://api.openweathermap.org/data/2.5/weather?q="+_city+"&appid=026b817616733447ff9cf585780ff7af",true();
-
-       request.onload=function (){
-           if (request.status >= 200 && request.status <400){
-             
-              var responseJSON = JSON.parse(request.responseText);
-             document.getElementById("result").innerHTML ="<h2>"+ responseJSON.name+ "</h2>"
-             document.getElementById("icon").innerHTML= "<img src=http://openweathermap.org/img/w/"+temporaire +".png />";
-      
-               document.getElementById("temperature").innerHTML=responseJSON.main.temp; 
-             
-               document.getElementById("cloud").innerHTML=responseJSON.clouds.all; 
-               document.getElementById("humidity").innerHTML= responseJSON.main.humidity + " % ";
-           }
-               else {
-                
-           }
-       };
-
-       request.onerror=function(){
-
-       };
-
-       request.send();
-                 }
 
 
-  function searchLatLng(_lat,_lng){  
-       console.log(searchLatLng,"Hello from "+_lat+","+_lng);    //A compléter dans la suite du TP
-         
-}
-  
     document.getElementById("gps").addEventListener("click", function(){  
-         
+              
             if (navigator.geolocation) {
               navigator.geolocation.getCurrentPosition(showPosition, showError);
               } else {
@@ -71,15 +37,6 @@ function showError(error) {
             break;
     }
 }
+ });
 
 }
-      
-
-
-
-
-
-
-
-
-
